88f9880f23ffc216118bc71d9514444f9ad8d005
WeberGrillen <oxholmf@gmail.com>
2026-01-29 10:34:38 +0100

    update

---
+++ 01._Miscellaneous/variables_1.js
@@ -0,0 +1,36 @@
+
+// Node REPL
+
+// type coercion
+// Strict equality
+
+// Rule 1: use const per default unless int needs to change, then use let
+
+// Const is constant in the assignment
+const luckyNumber = 4;
+
+console.log(luckyNumber);
+
+// JS Data types
+// String, Boolean, Number, BigInt, Objects, Symbol, null, undefined
+
+const person = {
+    // key-value pair
+    name: "Frederik",
+    age: 24
+};
+
+person.name = "GustavoRock"
+
+person.age = 112321;
+
+console.log(person)
+
+const people = [];
+
+people.push("Victor")
+
+console.log(people)
+
+// ASI (automatic semicolon insertion)
+
