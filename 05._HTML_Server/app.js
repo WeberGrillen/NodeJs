f1c945c666fdcf2b8720fdab5d975b9e0815af17
WeberGrillen <oxholmf@gmail.com>
2026-03-05 09:05:29 +0100

    Added PORT enviroment variable

--- 05._HTML_Server/app.js
+++ 05._HTML_Server/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 
 
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/frontpage.html')
 })
---
+++ 06._Online_Node_REPL/app.js
@@ -0,0 +1,37 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+
+import path from 'path';
+
+// ================== Pages ==================
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('public/frontend/frontend.html'));
+});
+
+// ================== API ==================
+
+
+app.post('/api/repl', (req, res) => {
+    let replCode = req.body?.replCode;
+
+    if (!replCode) {
+        return res.status(400).send({ errorMessage: 'Missing the key replCode in the JSON body' });
+    }
+    
+    replCode = replCode.replace('console.log("', '').replace('")', '');
+
+    res.send({ data: replCode });
+});
+
+console.log(process.env.NODE_ENV);
+
+const PORT = process.env.PORT || 8080;
+
+
+const server = app.listen(PORT, () => {
+    console.log("Server started on port", server.address().port);
+});
+
