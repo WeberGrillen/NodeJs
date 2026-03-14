ab50191ca170a0decdc57eef32fb592e157efb2a
WeberGrillen <oxholmf@gmail.com>
2026-03-14 21:29:30 +0100

    remove node_modules

--- 06._Online_Node_REPL/app.js
+++ 06._Online_Node_REPL/app.js
@@ -10,26 +10,44 @@ import path from 'path';
 // ================== Pages ==================
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve('public/frontend/frontend.html'));
+    res.sendFile(path.resolve('public/pages/frontend/frontend.html'));
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve('public/about/about.html'));
+    res.sendFile(path.resolve('public/pages/about/about.html'));
 })
 
 // ================== API ==================
 
+import { getOrCreateSandboxContext, executeCodeInSandbox } from './util/replUtil.js';
+
 
 app.post('/api/repl', (req, res) => {
-    let replCode = req.body?.replCode;
+    if (!req.body) {
+        return res.status(400).send({ errorMessage: 'Missing a JSON body' })
+    }
+
+    // let replCode = req.body?.replCode;
+    const { replCode, sandboxId } = req.body;
 
     if (!replCode) {
         return res.status(400).send({ errorMessage: 'Missing the key replCode in the JSON body' });
     }
-    
-    replCode = replCode.replace('console.log("', '').replace('")', '');
 
-    res.send({ data: replCode });
+    const sandbox = getOrCreateSandboxContext(sandboxId);
+
+    const { error, success, output, result } = executeCodeInSandbox(sandbox, replCode);
+
+    if (error) {
+        return res.status(500).send({
+            data: { error },
+            errorMessage: 'Error executing the provided code',
+        });
+    }
+    
+    // replCode = replCode.replace('console.log("', '').replace('")', '');
+
+    res.send({ data: { success, output, result } });
 });
 
 console.log(process.env.NODE_ENV);
